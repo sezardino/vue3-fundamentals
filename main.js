2f830d5d5a2b2221d7f89cf8f5d5cdcd0fdf8830
Edward Arechwa <e.arechwa@digitalcolliers.com>
2022-07-07 08:05:55 +0200

    lesson 8

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const app = Vue.createApp({
       product: "Socks",
       description: "Good socks for good legs",
       image: "./assets/images/socks_green.jpg",
-      inventory: 100,
+      inventory: 11,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
         { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
@@ -15,6 +15,11 @@ const app = Vue.createApp({
   },
   methods: {
     addToCart() {
+      if (this.inventory === 0) {
+        return;
+      }
+
+      this.inventory -= 1;
       this.cart += 1;
     },
     removeFromCart() {
@@ -22,6 +27,7 @@ const app = Vue.createApp({
         return;
       }
 
+      this.inventory += 1;
       this.cart -= 1;
     },
     changeImage(imageVariant) {
