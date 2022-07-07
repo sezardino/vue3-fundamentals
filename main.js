514661b4639466d59c6fb13fec837018e055300b
Edward Arechwa <e.arechwa@digitalcolliers.com>
2022-07-07 07:38:49 +0200

    lesson 4

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const app = Vue.createApp({
       product: "Socks",
       description: "Good socks for good legs",
       image: "./assets/images/socks_green.jpg",
+      inventory: 100
     };
   },
 });
