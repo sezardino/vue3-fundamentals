9ca56adc46ea946af744bc42ad824008c4629d12
Edward Arechwa <e.arechwa@digitalcolliers.com>
2022-07-10 10:03:39 +0200

    lesson 2

--- src/views/LoginForm.vue
+++ src/views/LoginForm.vue
@@ -1,6 +1,11 @@
 <template>
   <form @submit.prevent="onSubmit">
-    <BaseInput label="Email" type="email" />
+    <BaseInput
+      label="Email"
+      type="email"
+      v-model="emailValue"
+      :error="errorMessage"
+    />
 
     <BaseInput label="Password" type="password" />
 
@@ -9,18 +14,37 @@
 </template>
 
 <script>
+import { useField } from "vee-validate";
 import BaseInput from "@/components/BaseInput.vue";
 import BaseButton from "@/components/BaseButton.vue";
 
 export default {
   components: { BaseInput, BaseButton },
   setup() {
+    const emailValidator = (value) => {
+      if (!value) return "Email is required";
+
+      const emailRegexp =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (!emailRegexp.test(String(value).toLowerCase()))
+        return "Please enter a valid email address";
+
+      return true;
+    };
+
+    const { value: emailValue, errorMessage } = useField(
+      "email",
+      emailValidator
+    );
+
     function onSubmit() {
       alert("Submitted");
     }
 
     return {
       onSubmit,
+      emailValue,
+      errorMessage,
     };
   },
 };
