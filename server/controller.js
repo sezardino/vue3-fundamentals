3afacd6de459ef02191333e9d5b4da05513eae6f
Edward Arechwa <e.arechwa@digitalcolliers.com>
2022-07-19 20:01:33 +0200

    upd server functionality

--- server/controller.js
+++ server/controller.js
@@ -1,38 +1,45 @@
 const fs = require("fs");
 const jwt = require("jsonwebtoken");
 const events = require("../db/events.json");
+const users = require("../db/users.json");
 
 // The secret key should be an evironment variable in a live app
 const JWT_SECRET = "jwt_secret_key";
 
 const register = (req, res) => {
-  if (req.body) {
-    const user = {
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-      // You'll want to encrypt the password in a live app
-    };
+  const { name, email, password } = req.body;
 
-    var data = JSON.stringify(user, null, 2);
-
-    fs.writeFile("db/user.json", data, (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("Added user to user.json");
-      }
-    });
-
-    const token = jwt.sign({ user }, JWT_SECRET);
-    res.json({
-      token,
-      email: user.email,
-      name: user.name,
-    });
-  } else {
-    res.sendStatus(401);
+  if ((!name, !email, !password)) {
+    return res.sendStatus(400);
   }
+
+  if (users.find((user) => user.email === email)) {
+    return res.sendStatus(403);
+  }
+
+  const user = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    // You'll want to encrypt the password in a live app
+  };
+
+  const data = JSON.stringify([...users, user], null, 2);
+
+  fs.writeFile("db/users.json", data, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Added user to user.json");
+    }
+  });
+
+  const token = jwt.sign({ user }, JWT_SECRET);
+  res.json({
+    token,
+    email: user.email,
+    name: user.name,
+  });
 };
 
 const login = (req, res) => {
