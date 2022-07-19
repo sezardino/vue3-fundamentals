ef7e26355f8993d9f5bdaf4a71144356d2e236e9
Edward Arechwa <e.arechwa@digitalcolliers.com>
2022-07-19 21:02:24 +0200

    migrate to vue 3

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,23 @@
-import Vue from "vue";
-import Router from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/views/HomeView.vue";
 import Dashboard from "@/views/DashboardView.vue";
 
-Vue.use(Router);
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: Home,
+  },
+  {
+    path: "/dashboard",
+    name: "dashboard",
+    component: Dashboard,
+  },
+];
 
-const router = new Router({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: Home,
-    },
-    {
-      path: "/dashboard",
-      name: "dashboard",
-      component: Dashboard,
-    },
-  ],
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
