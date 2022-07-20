9e9cb78a1b6c509705f968c92ef2d4e7327552c2
Edward Arechwa <e.arechwa@digitalcolliers.com>
2022-07-20 20:30:06 +0200

    add error page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/views/HomeView.vue";
 import Dashboard from "@/views/DashboardView.vue";
+import ErrorView from "@/views/ErrorView.vue";
 
 const routes = [
   {
@@ -12,6 +13,12 @@ const routes = [
     path: "/dashboard",
     name: "dashboard",
     component: Dashboard,
+  },
+  {
+    path: "/:pathName()",
+    name: "error",
+    props: true,
+    component: ErrorView,
   },
 ];
 
---
+++ src/views/ErrorView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ status }}</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+defineProps({
+  message: {
+    type: String,
+    default: "Page not found",
+  },
+  status: {
+    type: Number,
+    default: 404,
+  },
+});
+</script>
+
+<style scoped></style>
